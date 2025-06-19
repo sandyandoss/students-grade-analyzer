bc78c2ac94537fca917ba3f7081c0c8f612258b8
Sandy Andos <1sandyandos@gmail.com>
2025-06-19 23:48:48 +0300

    initial commit

---
+++ Assignment.py
@@ -0,0 +1,8 @@
+# This is a python program to enter students' names and grades (out of 100) (the number of students should be decided by the user) and the program will do the following:
+# 1- Prints students names and grades
+# 2- Prints the average grade of the class
+# 3- Prints the highest grade earned (Student name and grade)
+# 4- Prints the count of students who passed (grade >= 60)
+
+# it includes loops, lists and functions.
+# the code have the following functions  display_student_summary ,  get_avg_grade ,  get_heighest_grade ,  count_passed 
